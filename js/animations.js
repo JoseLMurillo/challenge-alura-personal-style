ec509c8ec3cd39552f394a863dbd93214f7b2c71
JoseMurillo <joselillo072545@gmail.com>
2024-07-15 20:32:16 -0500

    Se agregaron los archivo js para animaciones y logica de encriptacion

---
+++ js/animations.js
@@ -0,0 +1,67 @@
+let currentIndex = 0;
+let codeIndex = 0;
+let randomIndex = 0;
+
+const listaAlura = ["a", "l", "u", "r", "a", ">"];
+const letras = ["AY", "BY", "CX", "DR", "EX", "DF", "GG", "TH", "IG", "JG", "KK", "LS", "MA", "CN", "ÑO", "YO", "AP", "XQ", "YR", "SI", "TU", "UV", "VA", "KW", "XD", "YB", "ZT"];
+const ipAddresses = [
+    "192.168.0.1",
+    "10.0.0.1",
+    "152.16.0.1",
+    "192.148.1.1",
+    "10.0.1.1",
+    "172.16.1.1",
+    "142.125.2.1",
+    "10.0.2.1",
+    "172.16.2.1",
+    "192.16.31.1"
+];
+
+let d = new Date();
+
+const dateInfo = {
+    local: d.toLocaleDateString(),
+    h: d.getHours(),
+    m: d.getMinutes()
+}
+
+const elements = {
+    logo: document.getElementById("logo"),
+    code: document.getElementById("code"),
+    hour: document.getElementById("hour"),
+    date: document.getElementById("date"),
+    random_font: document.getElementById("random-font"),
+    random_font2: document.getElementById("random-font2")
+}
+
+elements.date.textContent = dateInfo.local;
+elements.hour.textContent = `${dateInfo.h}:${dateInfo.m}`;
+
+function updateTime(){
+    dateInfo.m += 1;
+
+    if(dateInfo.m === 60){
+        dateInfo.m = 0;
+        dateInfo.h = (dateInfo.h + 1) % 24;
+    }
+    
+    elements.hour.textContent = `${dateInfo.h}:${dateInfo.m < 10 ? '0' : ''}${dateInfo.m}`;
+}
+
+function changeText() {
+    logo.textContent = listaAlura[currentIndex];
+    code.textContent = ipAddresses[codeIndex];
+
+    currentIndex = (currentIndex + 1) % listaAlura.length;
+    codeIndex = (codeIndex + 1) % ipAddresses.length;
+}
+
+function randomFont() {
+    elements.random_font.textContent = letras[randomIndex].at(0);
+    elements.random_font2.textContent = letras[randomIndex].at(1);
+    randomIndex = (randomIndex + 1) % letras.length;
+}
+
+setInterval(updateTime, 60000);
+setInterval(changeText, 500);
+setInterval(randomFont, 200);
---
+++ js/main.js
@@ -0,0 +1,297 @@
+const caracteresPermitidos = "abcdefghijklmnñopqrstuvwxyz ";
+let textoCorrecto = "";
+
+function copiar(){
+    let textarea = document.getElementById("message");
+    navigator.clipboard.writeText(textarea.value);
+
+    const snackbar = document.getElementById('snackbar');
+            snackbar.className = 'show';
+            setTimeout(() => {
+                snackbar.className = snackbar.className.replace('show', '');
+            }, 3000);
+}
+
+
+function validar() {
+    let textarea = document.getElementById("message");
+    let copy = document.getElementById("title");
+
+    if(textarea.value.length > 0){
+        copy.textContent = "Copy";
+        copy.setAttribute("class", "title");
+        copy.addEventListener("click", copiar);
+    } else {
+        copy.textContent = "ENCRIPTADOR";
+        copy.setAttribute("class", "");
+    }
+    
+    let textoCorrecto = '';
+
+    for (let i = 0; i < textarea.value.length; i++) {
+        let caracter = textarea.value[i];
+
+        for (let j = 0; j < caracteresPermitidos.length; j++) {
+            if (caracter == caracteresPermitidos[j]) {
+                textoCorrecto += caracter;
+            }
+        }
+    }
+
+    if (textoCorrecto !== textarea.value) {
+        textarea.value = textoCorrecto;
+    }
+}
+
+function encriptar() {
+    let MensajeNuevo = "";
+    let mensaje = document.getElementById("message").value;
+
+    if (mensaje.length <= 0) {
+        return;
+    }
+
+    let contador = 0;
+
+    while (contador < mensaje.length) {
+        if (mensaje[contador] == "e") {
+            MensajeNuevo += "enter";
+        } else if (mensaje[contador] == "i") {
+            MensajeNuevo += "imes";
+        } else if (mensaje[contador] == "a") {
+            MensajeNuevo += "ai";
+        } else if (mensaje[contador] == "o") {
+            MensajeNuevo += "ober";
+        } else if (mensaje[contador] == "u") {
+            MensajeNuevo += "ufat";
+        } else {
+            MensajeNuevo += mensaje[contador];
+        }
+
+        contador++;
+    }
+
+    mostrarEncriptado(MensajeNuevo);
+    changeColor();
+}
+
+function extraerCaracteres(inicio, fin, palabra) {
+    palabraNueva = "";
+
+    if (inicio >= fin) {
+        return;
+    }
+
+    while (inicio < fin) {
+        palabraNueva += palabra[inicio];
+        inicio++;
+    }
+
+    return palabraNueva;
+}
+
+function desencriptar() {
+    let mensaje = document.getElementById("message").value;
+    let contador = 0;
+    let mensajeNuevo = "";
+
+    while (contador < mensaje.length) {
+        if (extraerCaracteres(contador, contador + 5, mensaje) == "enter") {
+            mensajeNuevo += "e";
+            contador += 5;
+        } else
+            if (extraerCaracteres(contador, contador + 4, mensaje) == "imes") {
+                mensajeNuevo += "i";
+                contador += 4;
+            } else
+                if (extraerCaracteres(contador, contador + 2, mensaje) == "ai") {
+                    mensajeNuevo += "a";
+                    contador += 2;
+                } else
+                    if (extraerCaracteres(contador, contador + 4, mensaje) == "ober") {
+                        mensajeNuevo += "o";
+                        contador += 4;
+                    } else
+                        if (extraerCaracteres(contador, contador + 4, mensaje) == "ufat") {
+                            mensajeNuevo += "u";
+                            contador += 4;
+                        } else {
+                            mensajeNuevo += mensaje[contador];
+                            contador++;
+                        }
+    }
+
+    mostrarEncriptado(mensajeNuevo);
+    changeColor();
+}
+
+
+function mostrarEncriptado(mensaje){
+    document.getElementById("message").value = mensaje;
+}
+
+function changeColor(){
+    let clase = document.getElementById("danger");
+    clase.setAttribute("class", "stripe_inner stripe_no_standar");
+
+    setTimeout(() => {
+        let clase = document.getElementById("danger");
+        clase.setAttribute("class", "stripe_inner stripe_standar")
+    }, 1500);
+}
+
+/*COSAS A ARREGLAR
+Estilos del resultado en todos los dispositivos
+el boton desencriptar funciona aun cuando el input esta vacio
+Arreglar el contenido del input no se salga
+*/
+/*function copiarPortapapeles() {
+    if (document.getElementById("text-result").textContent.length > 0) {
+        navigator.clipboard.writeText(document.getElementById("text-result").textContent);
+
+        let x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    }
+}
+
+
+function activarBotones() {
+    document.getElementById("btnEncriptar").removeAttribute("disabled", "true");
+    document.getElementById("btnEncriptar").setAttribute("class", "button-active");
+
+    document.getElementById("btnDesencriptar").removeAttribute("disabled", "true");
+    document.getElementById("btnDesencriptar").setAttribute("class", "button-active");
+}
+
+function desActivarBotones() {
+    document.getElementById("btnEncriptar").removeAttribute("disabled", "false");
+    document.getElementById("btnEncriptar").removeAttribute("class");
+
+    document.getElementById("btnDesencriptar").removeAttribute("disabled", "false");
+    document.getElementById("btnDesencriptar").removeAttribute("class");
+}
+
+function inputVacio() {
+    if (document.getElementById("input-text").value.length > 0) {
+        activarBotones();
+    } else {
+        desActivarBotones();
+    }
+}
+
+function showEncript(palabra) {
+    document.getElementById("input-text").value = "";
+
+    document.getElementById("result").setAttribute("class", "result content-result");
+
+    document.getElementById("text-result").textContent = palabra;
+    document.getElementById("text-result").removeAttribute("hidden");
+
+    document.getElementById("default-result").setAttribute("hidden", true);
+
+    desActivarBotones();
+}
+
+function encriptar() {
+    document.getElementById("text-result").textContent = "";
+    let palabra = document.getElementById("input-text").value;
+
+    if (palabra.length <= 0) {
+        return;
+    }
+
+    let contador = 0;
+
+    let palabraNueva = "";
+
+    while (contador < palabra.length) {
+        if (palabra[contador] == "e") {
+            palabraNueva += "enter";
+        } else if (palabra[contador] == "i") {
+            palabraNueva += "imes";
+        } else if (palabra[contador] == "a") {
+            palabraNueva += "ai";
+        } else if (palabra[contador] == "o") {
+            palabraNueva += "ober";
+        } else if (palabra[contador] == "u") {
+            palabraNueva += "ufat";
+        } else {
+            palabraNueva += palabra[contador];
+        }
+
+        contador++;
+    }
+
+    showEncript(palabraNueva);
+}
+
+function extraerCaracteres(inicio, fin, palabra) {
+    let contador = inicio;
+    palabraNueva = "";
+
+    if (inicio >= fin) {
+        return;
+    }
+
+    while (contador < fin) {
+        palabraNueva += palabra[contador];
+        contador++;
+    }
+
+    return palabraNueva;
+}
+
+function desencriptar() {
+    document.getElementById("text-result").textContent = "";
+    let palabra = document.getElementById("input-text").value;
+    let contador = 0;
+    let palabraNueva = "";
+
+    while (contador < palabra.length) {
+        if (palabra[contador] == "e" && extraerCaracteres(contador, contador + 5, palabra) == "enter") {
+            palabraNueva += "e";
+            contador += 5;
+        } else
+            if (palabra[contador] == "i" && extraerCaracteres(contador, contador + 4, palabra) == "imes") {
+                palabraNueva += "i";
+                contador += 4;
+            } else
+                if (palabra[contador] == "a" && extraerCaracteres(contador, contador + 2, palabra) == "ai") {
+                    palabraNueva += "a";
+                    contador += 2;
+                } else
+                    if (palabra[contador] == "o" && extraerCaracteres(contador, contador + 4, palabra) == "ober") {
+                        palabraNueva += "o";
+                        contador += 4;
+                    } else
+                        if (palabra[contador] == "u" && extraerCaracteres(contador, contador + 4, palabra) == "ufat") {
+                            palabraNueva += "u";
+                            contador += 4;
+                        } else {
+                            palabraNueva += palabra[contador];
+                            contador++;
+                        }
+    }
+
+
+    return showEncript(palabraNueva);
+}*/
+
+/*Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+Requisitos:
+
+Debe funcionar solo con letras minúsculas
+No deben ser utilizados letras con acentos ni caracteres especiales
+Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+Por ejemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+
+La página debe tener campos para
+inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
+El resultado debe ser mostrado en la pantalla.
+Extras:
+
+Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.*/
